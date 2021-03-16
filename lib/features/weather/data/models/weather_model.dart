51c9f727c53435aa77126ba84651b5910a279bd7
EtNinja <annealmd@gmail.com>
2021-03-16 19:17:18 -0300

    :construction: Data Layer
    added  model file

---
+++ lib/features/weather/data/models/weather_model.dart
@@ -0,0 +1,27 @@
+import 'package:meta/meta.dart';
+import 'package:weather_app/features/weather/domain/entities/weather.dart';
+
+class WeatherModel extends Weather {
+  WeatherModel({
+    @required String city,
+    @required String temperature,
+    @required String time,
+  }) : super(city: city, temperature: temperature, time: time);
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    return WeatherModel(
+      city: json['city'],
+      temperature: json['temperatura'],
+      time: json['time'],
+      //DateTime.parse(json['time']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'city': city,
+      'temperature': temperature,
+      'time': time,
+    };
+  }
+}
